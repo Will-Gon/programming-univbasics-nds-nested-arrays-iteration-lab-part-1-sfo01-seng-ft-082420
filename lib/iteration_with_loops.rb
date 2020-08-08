add87107c280535a5e7738391a51a16eae099268
Will Gonzalez <will.colocho@gmail.com>
2020-08-08 21:25:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,8 @@ def find_even_values(src)
 row_index = 0 
 while row_index < find_even_values.count do 
   element_index = 0 
-  while element_index.even? < find_even_values[row_index].count do
-    p find_even_values[row_index][element_index]
+  while element_index < find_even_values[row_index].count do
+    p find_even_values[row_index][element_index.even?]
     element_index += 1 
   end 
   row_index += 1 
